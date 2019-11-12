61eef22bba45c21cb3e122948d18f6c99f8eb07f
Wentworthliu123 <wentworthliu@pku.edu.cn>
2019-11-12 08:54:49 -0600

    Add SMB and cumulative return plots

--- five_factor_model/HF_Package/Functions/Python_realization/Mat_Python_comparison.py
+++ five_factor_model/HF_Package/Functions/Python_realization/Mat_Python_comparison.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[80]:
+# In[38]:
 
 
 import hdf5storage
 import pandas as pd
 import datetime
 
-mat = hdf5storage.loadmat('step4_20150901_20150901_v3.mat')
+mat = hdf5storage.loadmat('step4_20150901_20150930_v3.mat')
 matdata = pd.DataFrame(mat['return_matrix'],columns = ['Date','Time','matSMB','matHML','matRMW','matCMA','matMOM','matMarket'])
 #Please use HDF reader for matlab v7.3 files
 
@@ -16,33 +16,52 @@ CSV_FILE_PATH = 'F:\\RA_Fama_French_Factor\\five_factor_model\\HF_Package\\Outpu
 pydata = pd.read_csv(CSV_FILE_PATH)
 
 DataPc=pydata.copy()
-date=1
+date=21
 DataPc=DataPc.iloc[:date*79,:]
 DataPc=DataPc.set_index('time')
 matdata=matdata.set_index(DataPc.index)
 comparison=matdata.join(DataPc)
+comparison.drop(columns=['Date','Time'],inplace=True)
 
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import pandas as pd
-
+import numpy as np
+tick_spacing = 395
 # gca stands for 'get current axis'
 # comparison.reset_index(inplace=True)
-fig, axes = plt.subplots(nrows=5, ncols=1, figsize=(12,15))
+fig, axes = plt.subplots(nrows=6, ncols=1, figsize=(12,15))
 
+for x in axes:
+    x.xaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
 comparison.plot(y=['matHML','HML'],ax=axes[0])
 comparison.plot(y=['matRMW','RMW'],ax=axes[1])
 comparison.plot(y=['matCMA','CMA'],ax=axes[2])
 comparison.plot(y=['matMOM','MOM'],ax=axes[3])
 comparison.plot(y=['matMarket','Rm'],ax=axes[4])
+comparison.plot(y=['matSMB','SMB'],ax=axes[5])
+
+figc, axesc = plt.subplots(nrows=6, ncols=1, figsize=(12,15))
+comparisoncum=(comparison+1).cumprod()-1
+
+for x in axesc:
+    x.xaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
+comparisoncum.plot(y=['matHML','HML'],ax=axesc[0])
+comparisoncum.plot(y=['matRMW','RMW'],ax=axesc[1])
+comparisoncum.plot(y=['matCMA','CMA'],ax=axesc[2])
+comparisoncum.plot(y=['matMOM','MOM'],ax=axesc[3])
+comparisoncum.plot(y=['matMarket','Rm'],ax=axesc[4])
+comparisoncum.plot(y=['matSMB','SMB'],ax=axesc[5])
+
 
 
 plt.show()
 
-
-# In[93]:
-
-
-
+from matplotlib.backends.backend_pdf import PdfPages
+pp = PdfPages("mat_py_one_month.pdf")
+pp.savefig(fig)
+pp.savefig(figc)
+pp.close()
 
 
 # In[ ]:
@@ -51,10 +70,29 @@ plt.show()
 
 
 
+# In[37]:
+
+
+
+
+
+# In[33]:
+
+
+
+
+
+# In[36]:
+
+
+axes
+
+
 # In[76]:
 
 
-
+fig2, axes2 = plt.subplots(nrows=6, ncols=1, figsize=(12,15))
+plt.plot((_ffcomp + 1).cumprod() - 1)
 
 
 # In[77]:
